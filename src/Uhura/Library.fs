c427b48644533cd007978bafbf7f79f33bcc6e0c
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2016-12-24 18:16:44 -0500

    Initial router

--- src/Uhura/Library.fs
+++ src/Uhura/Library.fs
@@ -1,16 +1,47 @@
 namespace Uhura
 
-/// Documentation for my library
-///
-/// ## Example
-///
-///     let h = Library.hello 1
-///     printfn "%d" h
-///
-module Library = 
-  
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+module Web =
+        module Http =
+            module Methods =
+                [<Literal>] 
+                let GET = "GET"
+                [<Literal>] 
+                let POST = "POST"
+                [<Literal>] 
+                let PUT = "PUT"
+                [<Literal>] 
+                let DELETE = "DELETE"
+                [<Literal>] 
+                let HEAD = "HEAD"
+                [<Literal>] 
+                let OPTIONS = "OPTIONS"
+                [<Literal>] 
+                let CONNECT = "CONNECT"
+        module Routing =
+            open Hopac
+            open Microsoft.AspNetCore.Builder
+            open Microsoft.AspNetCore.Http
+            open System.Threading.Tasks
+
+            type RouteHandler = HttpContext -> Job<unit>
+            type Route = {
+                Method : string
+                Path : string
+                Handler : RouteHandler
+            }
+
+            let inline toLowerInvariant (path : string) =
+                path.ToLowerInvariant()
+            let normalize = toLowerInvariant
+            let inline matches route (httpContext : HttpContext)=
+                httpContext.Request.Method = route.Method && (normalize httpContext.Request.Path.Value) = (normalize route.Path)
+            let inline unitTaskToTask (task : Task<unit>) =
+                task :> Task
+            let inline applyRoutes (appBuilder : IApplicationBuilder) routes =
+                routes
+                |> Seq.iter(fun route ->
+                    appBuilder.MapWhen(
+                        (fun httpContext -> matches route httpContext), 
+                        fun app -> app.Run (fun a -> (a |> route.Handler |> Hopac.startAsTask |> unitTaskToTask ))) 
+                    |> ignore
+                )
