05ae27089b4699497d79c84141617ff91294f482
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2016-12-24 18:48:47 -0500

    Use Uhura routing in example

--- examples/kestrel/Program.fs
+++ examples/kestrel/Program.fs
@@ -4,13 +4,28 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
 
+open Uhura.Web
+open Uhura.Web.Routing
+open Hopac
+let helloWorldHandler (ctx : HttpContext) = job {
+    do! ctx.Response.WriteAsync("Hello world") |> Job.awaitUnitTask
+}
+
 
 [<EntryPoint>]
 let main argv =
+    let buildRoutes appBuilder =
+        let routes =
+            [
+                { Method = Http.Methods.GET; Path = "/"   ; Handler = helloWorldHandler }
+             
+            ]
+
+        routes |> applyRoutes appBuilder
     WebHostBuilder()
         .UseUrls("http://localhost:8080")
         .UseKestrel()
-        .Configure(fun a -> a.Run(fun c -> c.Response.WriteAsync("Hello from Plain Kestrel!")))
+        .Configure(fun a -> buildRoutes a)
         .Build()
         .Run()
 
